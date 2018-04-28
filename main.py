469594f59c8e1e187b811378b5cf011e6a414f3b
velmisov <velmisovx@gmail.com>
2018-04-28 13:39:42 +0300

    open SUMO with simple data

--- main.py
+++ main.py
@@ -11,3 +11,19 @@ import data.simple.route
 
 data.simple.route.generate()
 
+port = 10000
+
+import subprocess
+import traci
+
+sumoProcess = subprocess.Popen(['sumo-gui.exe', "-c", "data\simple\simple.sumocfg", "--remote-port", str(port)],
+                               stdout=sys.stdout, stderr=sys.stderr)
+traci.init(port)
+
+step = 0
+while traci.simulation.getMinExpectedNumber() > 0:
+    traci.simulationStep()
+    print(step)
+    step += 1
+
+traci.close()
