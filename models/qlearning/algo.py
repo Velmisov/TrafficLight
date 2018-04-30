1cbe5127999b72f58d9aefbd67db917f81799362
velmisov <velmisovx@gmail.com>
2018-05-01 01:42:03 +0300

    fit base

--- models/qlearning/algo.py
+++ models/qlearning/algo.py
@@ -1,13 +1,33 @@
+import sys
+import subprocess
+
+import traci
+
+import settings
+from data.simple.route import Route
 import models.qlearning.discrete_state
 
 
 class QLearning:
 
-    def __init__(self):
-        pass
+    def __init__(self, path: str):
+        self.path = path
 
-    def fit(self):
-        pass
+    def fit(self, number_of_days: int):
+        port = 5000
+        route = Route(settings.CARS_IN_DAY)
+        for day in range(number_of_days):
+            route.next()
+            sumo_process = subprocess.Popen(['sumo.exe', "--waiting-time-memory=1000000",
+                                             "-c", self.path+".sumocfg", "--remote-port", str(port)],
+                                            stdout=sys.stdout, stderr=sys.stderr)
+            traci.init(port)
 
-    def predict(self):
+            while traci.simulation.getMinExpectedNumber() > 0:
+                traci.simulationStep()
+
+            traci.close()
+            sumo_process.kill()
+
+    def predict(self) -> int:
         pass
