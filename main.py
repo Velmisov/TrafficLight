b7ab1c28ead15abcdd98e4de817d15bb77a0d407
velmisov <velmisovx@gmail.com>
2018-04-30 17:55:24 +0300

    parser does all work

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 import subprocess
 import traci
 import data.simple.route
-from outparsing import Parser
+import outparsing
 from state import State
 import pandas as pd
 
@@ -13,31 +13,23 @@ if 'SUMO_HOME' in os.environ:
 else:
     sys.exit("Undeclared environment variable 'SUMO_HOME'")
 
-route = data.simple.route.Route(1000)
+route = data.simple.route.Route(200)
+parser = outparsing.Parser(route.edges)
 
 port = 10000
 
-for i in range(1):
+for i in range(3):
     route.next()
     sumoProcess = subprocess.Popen(['sumo-gui.exe', "-c", "data\simple\simple.sumocfg", "--remote-port", str(port)],
                                    stdout=sys.stdout, stderr=sys.stderr)
     traci.init(port)
 
-    number_of_vehicles = pd.DataFrame(columns=route.edges)
-    waiting_time = pd.DataFrame(data=[], columns=route.edges)
-    # total_waiting_time = pd.DataFrame(data=[], columns=route.edges)
-
     while traci.simulation.getMinExpectedNumber() > 0:
-        number_of_vehicles = number_of_vehicles.append(State.get_number_of_vehicles(route.edges), ignore_index=True)
-        waiting_time = waiting_time.append(State.get_waiting_time(route.edges), ignore_index=True)
-        # total_waiting_time = total_waiting_time.append(current_total_time, ignore_index=True)
-
+        parser.step()
         traci.simulationStep()
-
-    Parser.plot_number_of_vehicles(number_of_vehicles)
-    Parser.plot_waiting_time(waiting_time)
-    # Parser.plot_total_waiting_time(total_waiting_time)
 
     traci.close()
 
+    parser.get_statistics()
+    parser.clear()
     port += 1
--- outparsing.py
+++ outparsing.py
@@ -1,35 +1,40 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from state import State
 
 
 class Parser:
 
-    def __init__(self):
-        pass
+    def __init__(self, edges):
+        self.edges = edges
+        self.number_of_vehicles = pd.DataFrame(columns=edges)
+        self.waiting_time = pd.DataFrame(columns=edges)
 
-    @staticmethod
-    def plot_waiting_time(waiting_time: pd.DataFrame):
-        waiting_time.groupby(waiting_time.index).sum().plot()
+    def plot_waiting_time(self):
+        self.waiting_time.groupby(self.waiting_time.index).sum().plot()
 
         plt.xlabel("Total time")
         plt.ylabel("Waiting time")
         plt.title("Waiting time for edges")
         plt.show()
 
-    @staticmethod
-    def plot_number_of_vehicles(number: pd.DataFrame):
-        number.groupby(number.index).sum().plot()
+    def plot_number_of_vehicles(self):
+        self.number_of_vehicles.groupby(self.number_of_vehicles.index).sum().plot()
 
         plt.xlabel("Total time")
         plt.ylabel("Number of vehicles")
         plt.title("Number of vehicles for edges")
         plt.show()
 
-    @staticmethod
-    def plot_total_waiting_time(total_waiting_time: pd.DataFrame):
-        total_waiting_time.groupby(total_waiting_time.index).sum().plot()
+    def step(self):
+        self.number_of_vehicles = self.number_of_vehicles.append(State.get_number_of_vehicles(self.edges),
+                                                                 ignore_index=True)
+        self.waiting_time = self.waiting_time.append(State.get_waiting_time(self.edges), ignore_index=True)
 
-        plt.xlabel("Total time")
-        plt.ylabel("Total waiting time")
-        plt.title("Total waiting time for edges")
-        plt.show()
+    def get_statistics(self):
+        self.plot_number_of_vehicles()
+        self.plot_waiting_time()
+
+    def clear(self):
+        self.number_of_vehicles = pd.DataFrame(columns=self.edges)
+        self.waiting_time = pd.DataFrame(columns=self.edges)
