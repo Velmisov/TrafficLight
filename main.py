f77d48a453616d7e824bda3ab5e282e5b2bcf1da
velmisov <velmisovx@gmail.com>
2018-05-02 19:10:44 +0300

    it works somehow tested only on one-way routes

--- main.py
+++ main.py
@@ -16,35 +16,50 @@ if 'SUMO_HOME' in os.environ:
 else:
     sys.exit("Undeclared environment variable 'SUMO_HOME'")
 
-q = QLearning(info, Route(settings.CARS_IN_DAY * 10), 0.5, 0.1)
+q = QLearning(info, Route(settings.CARS_IN_DAY * 30), 0.8, 0.1)
 q.fit(1)
 
-# port = 8813
-#
-# route = Route(100)
-# parser = Parser(info.EDGES)
-#
-# route.next()
-# sumo_process = subprocess.Popen(['sumo-gui.exe', settings.WAITING_TIME_MEMORY_LIMIT,
-#                                  "-c", "data\simple\simple.sumocfg", "--remote-port", str(port)],
-#                                 stdout=sys.stdout, stderr=sys.stderr)
-# traci.init(port)
-#
-# step = 0
-# sec_in_phase = 0
-# while traci.simulation.getMinExpectedNumber() > 0:
-#     # print(traci.trafficlight.getPhase("tl"))
-#     # if traci.trafficlight.getPhase("tl") == 0 and sec_in_phase == 0:
-#     #     traci.trafficlight.setPhaseDuration("tl", 10)
-#     # elif traci.trafficlight.getPhase("tl") != 0:
-#     #     sec_in_phase = -1
-#     parser.step()
-#     traci.simulationStep()
-#     step += 1
-#     sec_in_phase += 1
-#
-# traci.close()
-# sumo_process.kill()
-#
-# parser.get_statistics()
-# parser.clear()
+route = Route(settings.CARS_IN_DAY)
+route.set_coefficients([0.25, 0.25, 0.25, 0.25, 0., 0., 0., 0.])
+port = 8814
+
+parser = Parser(info.EDGES)
+
+route.next()
+sumo_process = subprocess.Popen(['sumo-gui.exe', settings.WAITING_TIME_MEMORY_LIMIT,
+                                 "-c", "data\simple\simple.sumocfg", "--remote-port", str(port)],
+                                stdout=sys.stdout, stderr=sys.stderr)
+traci.init(port)
+
+step = 0
+current_phase = int(traci.trafficlight.getPhase(info.TL))
+sec_in_phase = 0
+queue_tracker = {}
+waiting_tracker = {}
+vehicles_tracker = {}
+while traci.simulation.getMinExpectedNumber() > 0:
+    if current_phase == int(traci.trafficlight.getPhase(info.TL)) and step != 0:
+        time_current_phase += 1
+    else:
+        time_current_phase = 0
+        current_phase = int(traci.trafficlight.getPhase(info.TL))
+
+    if current_phase in info.GREEN_PHASES and time_current_phase == 0:
+
+        for edge in info.EDGES:
+            queue_tracker[edge] = traci.edge.getLastStepHaltingNumber(edge)
+            waiting_tracker[edge] = traci.edge.getWaitingTime(edge)
+            vehicles_tracker[edge] = traci.edge.getLastStepVehicleNumber(edge)
+
+        traci.trafficlight.setPhaseDuration(info.TL, q.predict(current_phase, queue_tracker, waiting_tracker,
+                                                               vehicles_tracker))
+    parser.step()
+    traci.simulationStep()
+    step += 1
+    sec_in_phase += 1
+
+traci.close()
+sumo_process.kill()
+
+parser.get_statistics()
+parser.clear()
--- models/qlearning/algo.py
+++ models/qlearning/algo.py
@@ -1,6 +1,7 @@
 import sys
 import subprocess
-import pandas as pd
+import numpy as np
+import random
 
 import traci
 
@@ -15,39 +16,105 @@ class QLearning:
         self.route = route
         self.learning_rate = learning_rate
         self.discount_factor = discount_factor
-        self.q_value = pd.DataFrame(columns=range(settings.MIN_GREEN_TIME, settings.MAX_GREEN_TIME))
 
         self.discrete_state = DiscreteState(info, route)
         self.discrete_state.fit(20)
 
+        self.q_value = np.zeros((self.discrete_state.total_number_of_states,
+                                 settings.MAX_GREEN_TIME - settings.MIN_GREEN_TIME))
+
     def __print_q(self):
         print(self.q_value)
 
-    def __reward(self, state, action) -> float:
-        pass
+    def __compute_reward(self, queue_tracker, waiting_tracker):
+        reward = 0
+        for edge in self.info.EDGES:
+            reward -= ((1 * queue_tracker[edge]) ** 1.75 + (2 * waiting_tracker[edge]) ** 1.75)
+        return reward
 
-    def __update(self, state, next_state, action):
-        reward_for_state_action = self.__reward(state, action)
-        prev_q = self.q_value.iloc[state, action]
-        self.q_value[state, action] = prev_q + self.learning_rate * (reward_for_state_action + self.discount_factor *
+    def __update(self, state, next_state, action, reward):
+        prev_q = self.q_value[state, action]
+        self.q_value[state, action] = prev_q + self.learning_rate * (reward + self.discount_factor *
                                                                      max(self.q_value[next_state, :] - prev_q))
-        return self.__reward(state, action)
 
     def fit(self, number_of_days: int):
-        pass
-        # port = 8813
-        # for day in range(number_of_days):
-        #     self.route.next()
-        #     sumo_process = subprocess.Popen(['sumo.exe', settings.WAITING_TIME_MEMORY_LIMIT,
-        #                                      "-c", self.info.PATH+".sumocfg", "--remote-port", str(port)],
-        #                                     stdout=sys.stdout, stderr=sys.stderr)
-        #     traci.init(port)
-        #
-        #     while traci.simulation.getMinExpectedNumber() > 0:
-        #         traci.simulationStep()
-        #
-        #     traci.close()
-        #     sumo_process.kill()
+        port = 8813
 
-    def predict(self) -> int:
-        pass
+        queue_tracker = {}
+        waiting_tracker = {}
+        vehicles_tracker = {}
+        for edge in self.info.EDGES:
+            queue_tracker[edge] = 0
+            waiting_tracker[edge] = 0
+            vehicles_tracker[edge] = 0
+
+        temperature = 1.
+        for day in range(number_of_days):
+            self.route.set_coefficients([0.25, 0.25, 0.25, 0.25, 0., 0., 0., 0.])
+            self.route.next()
+            sumo_process = subprocess.Popen(['sumo-gui.exe', settings.WAITING_TIME_MEMORY_LIMIT,
+                                             "-c", self.info.PATH+".sumocfg", "--remote-port", str(port)],
+                                            stdout=sys.stdout, stderr=sys.stderr)
+            traci.init(port)
+
+            step = 0
+            current_phase = int(traci.trafficlight.getPhase(self.info.TL))
+            time_current_phase = 0
+            current_state = -1
+            last_state = -1
+            current_action = -1
+            last_action = -1
+            current_reward = -1
+            last_reward = -1
+            while traci.simulation.getMinExpectedNumber() > 0:
+
+                if current_phase == int(traci.trafficlight.getPhase(self.info.TL)) and step != 0:
+                    time_current_phase += 1
+                else:
+                    time_current_phase = 0
+                    current_phase = int(traci.trafficlight.getPhase(self.info.TL))
+
+                if current_phase in self.info.GREEN_PHASES and time_current_phase == 0:
+
+                    for edge in self.info.EDGES:
+                        queue_tracker[edge] = traci.edge.getLastStepHaltingNumber(edge)
+                        waiting_tracker[edge] = traci.edge.getWaitingTime(edge)
+                        vehicles_tracker[edge] = traci.edge.getLastStepVehicleNumber(edge)
+
+                    current_state = self.discrete_state.get_state(current_phase, queue_tracker, waiting_tracker,
+                                                                  vehicles_tracker)
+                    current_reward = self.__compute_reward(queue_tracker, waiting_tracker)
+
+                    if step != 0:
+                        self.__update(last_state, current_state, last_action, last_reward)
+
+                    if random.random() < temperature or np.sum(self.q_value[current_state, :]) == 0:
+                        current_action = random.randint(0, self.q_value.shape[1] - 1)
+                    else:
+                        arg_max_array = np.argmax(self.q_value[current_state])
+                        if type(arg_max_array) == np.int64:
+                            arg_max_array = [arg_max_array]
+                        else:
+                            print(arg_max_array)
+                        current_action = arg_max_array[random.randint(0, len(arg_max_array) - 1)]
+
+                    traci.trafficlight.setPhaseDuration(self.info.TL, settings.MIN_GREEN_TIME + current_action)
+                    last_state = current_state
+                    last_action = current_action
+                    last_reward = current_reward
+                    temperature = max(0.01, temperature - 0.001)
+
+                traci.simulationStep()
+                step += 1
+
+            traci.close()
+            sumo_process.kill()
+        print(self.q_value)
+        print(temperature)
+
+    def predict(self, phase, queue, waiting, vehicles) -> int:
+        state = self.discrete_state.get_state(phase, queue, waiting, vehicles)
+        arg_max_array = np.argmax(self.q_value[state])
+        if type(arg_max_array) == np.int64:
+            arg_max_array = [arg_max_array]
+        return arg_max_array[random.randint(0, len(arg_max_array) - 1)]
